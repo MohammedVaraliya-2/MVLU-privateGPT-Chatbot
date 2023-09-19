1b642dc5bfe9e1f8eeb5461f171afe704d5f4bb8
Mohammedvaraliya <mohammedvaraliya2661392@gmail.com>
2023-09-19 12:29:19 +0530

    implemet the new file uploading logic

--- app/views.py
+++ app/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import os
 from .privateGPT_logic import get_response
 import asyncio
+from .ingest_new_file import main  # Import the ingestion logic
+
+def home(request):
+    if request.method == 'POST' and request.FILES.get('file'):
+        # Save the uploaded file to a temporary location
+        uploaded_file = request.FILES['file']
+        file_path = os.path.join('uploads', uploaded_file.name)
+        with open(file_path, 'wb') as destination:
+            for chunk in uploaded_file.chunks():
+                destination.write(chunk)
+
+        # Process the uploaded file using the ingestion logic
+        main()  # Run the ingestion logic
+
+        # Redirect the user to the chatbot page after ingestion
+        return redirect('chatbot')
+
+    return render(request, 'home.html')
 
 # Define an asynchronous view to handle chatbot interactions
 async def chatbot(request):
+
+    ingested_data = request.session.get('ingested_data', None)
+
     if request.method == 'POST':
         # Get the user's question from the form
         user_question = request.POST.get('input_text', '')
